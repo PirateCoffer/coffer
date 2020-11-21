e964dfe2ad5cc6713c54823cb0530f2d1ede0373
Aaron <aronanda@gmail.com>
2020-11-21 13:15:35 -0800

    fixed dependencies

--- src/plugins/plugins.client.js
+++ src/plugins/plugins.client.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueClipboard from 'vue-clipboard2'
 import utilMixins from '~/mixins/utilMixins'
 
-require('iron-core').explode()
+require('iron-common').explode()
 
 Vue.use(function (Vue, options) {
   Vue.mixin({ mixins: [ utilMixins ] })
@@ -25,29 +25,4 @@ export default async ({ store }, inject) => {
   }
   if (record)
     store.commit('user/SET_USER', record)
-
-  // ctx.$cookie = process.browser 
-  //   ? require('~/services/cookie/client-cookie').default
-  //   : require('~/services/cookie/server-cookie')
-  // inject('cookie', ctx.$cookie)
-  // let csrf = _.get(ctx, 'req.xsrf')
-  // if (process.server) {
-    // ctx.beforeNuxtRender(({ nuxtState, Components }) => {
-    //   let error = _.get(ctx, 'req.error')
-    //   if (error) {
-    //     return ctx.error({ statusCode: 403, message: error })
-    //   }
-    //   nuxtState.token = csrf
-    // })
-    // ctx.app.$axios.defaults.headers.post['xsrf-token'] = ctx.req.xsrf
-    // if (ctx.req.user) {
-    //   ctx.store.commit('user/SET_USER', ctx.req.user, { root: true })
-    // }
-  // } else if (process.browser) {
-    // const storage = require('~/services/storage').default
-    // ctx.$storage = storage
-    // inject('storage', storage)
-    // // Auth:
-    // ctx.app.$axios.defaults.headers.post['xsrf-token'] = ctx.nuxtState.token
-  // }
 }
